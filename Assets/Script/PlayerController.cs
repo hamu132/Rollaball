e52a2713c88d1b6bc268f604bb46c89ee59d3e4f
gurahamu <guba06120@gmail.com>
2025-12-21 11:10:05 +0900

    移動モーションをいい感じにした

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Build.Content;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private float movementX;
-    private float movementY;
-    public float speed = 0;
     private int count;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    //現在の速度、激しい初速、収束後の速度、速度変化割合
+    private float m_currentSpeed;
+    [SerializeField] private float m_initialSpeed;
+    [SerializeField] private float m_normalSpeed;
+    [SerializeField] private float m_decayRate;
+    private bool m_isInitialDash;
+    //方向ベクトル
+    private Vector3 m_moveInput;
+    private Vector2 m_previousInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,14 +36,49 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        if (m_isInitialDash)
+        {
+            m_currentSpeed = Mathf.Lerp(m_currentSpeed, m_normalSpeed, m_decayRate * Time.fixedDeltaTime);
+            Vector3 direction = new Vector3 (m_moveInput.x, 0, m_moveInput.y);
+            direction *= m_currentSpeed;
+            direction.y = rb.velocity.y;
+            rb.velocity = direction;
+        }
+        else
+        {
+            rb.velocity *= 0.9f;
+        }
     }
-    void OnMove(InputValue movementValue)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        // 1. 入力が始まった瞬間 (started)
+        if (context.started)
+        {
+            m_isInitialDash = true;
+            m_currentSpeed = m_initialSpeed;
+            m_moveInput = context.ReadValue<Vector2>();
+            m_previousInput = m_moveInput;
+        }
+
+        // 2. 入力が継続中 (performed)
+        // 押したときは通過話した時は通過しないようにしたい
+        if (context.performed)
+        {
+            //縦横→ナナメの場合だけ発動
+            if(m_previousInput.x * m_previousInput.y == 0)
+            {
+                m_currentSpeed = m_initialSpeed;
+            }
+            m_moveInput = context.ReadValue<Vector2>();
+            m_previousInput = m_moveInput;
+        }
+
+        // 3. 入力が終わった瞬間 (canceled)
+        if (context.canceled)
+        {
+            m_isInitialDash = false;
+        }
+        //Debug.Log($"{context.started}, {context.performed}, {context.canceled}");
     }
     void SetCountText()
     {
